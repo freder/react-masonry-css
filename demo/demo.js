cfcab573ce1ccfd766a424bd5f5f2fd2c5b4803e
Frederic Brodbeck <freder.code@gmail.com>
2017-10-16 22:45:49 +0200

    tabs

--- demo/demo.js
+++ demo/demo.js
@@ -6,44 +6,44 @@ const Masonry = require('../lib/ReactMasonry')
 //import {Masonry} from '../lib-es5/ReactMasonry'
 
 class App extends React.Component {
-  constructor (props) {
-    super(props);
+	constructor (props) {
+		super(props);
 
-    this.state = {};
-  }
+		this.state = {};
+	}
 
-  render () {
-    var breakpointCols = [
-      [1, '(max-width: 500px)'],
-      [2, '(max-width: 700px)'],
-      [3, '(max-width: 1100px)'],
-      [4, '(min-width: 1101px)'],
-    ];
+	render () {
+		var breakpointCols = [
+			[1, '(max-width: 500px)'],
+			[2, '(max-width: 700px)'],
+			[3, '(max-width: 1100px)'],
+			[4, '(min-width: 1101px)'],
+		];
 
-    var heights = [100, 500, 300, 100, 200, 500, 300, 200, 100, 100];
+		var heights = [100, 500, 300, 100, 200, 500, 300, 200, 100, 100];
 
-    return (
-      <div>
-        <Masonry
-          breakpointCols={breakpointCols}
-          heights={heights}
-          className="my-masonry-grid"
-          columnClassName="my-masonry-grid_column"
-        >
-          {heights
-            .map((h, i) => {
-              return <div 
-                style={{ height: h }}
-                key={`${i}-${h}`}
-              >
-                {i}. {h}
-              </div>
-            })
-          }
-        </Masonry>
-      </div>
-    );
-  }
+		return (
+			<div>
+				<Masonry
+					breakpointCols={breakpointCols}
+					heights={heights}
+					className="my-masonry-grid"
+					columnClassName="my-masonry-grid_column"
+				>
+					{heights
+						.map((h, i) => {
+							return <div 
+								style={{ height: h }}
+								key={`${i}-${h}`}
+							>
+								{i}. {h}
+							</div>
+						})
+					}
+				</Masonry>
+			</div>
+		);
+	}
 }
 
 ReactDom.render(<App />, document.getElementById('app'))
